37955637964ccbf563eafd5ef5a6daacd4b95d7b
user42 <lina.noirot@gmail.com>
2020-05-04 16:30:16 +0200

    fix lstnew

--- list/ft_lstnew.c
+++ list/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 21:35:14 by user42            #+#    #+#             */
-/*   Updated: 2020/05/02 17:42:16 by user42           ###   ########.fr       */
+/*   Updated: 2020/05/04 16:29:02 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	if (!(new = malloc(sizeof(t_list *))))
+	if (!(new = malloc(sizeof(t_list))))
 		return (NULL);
 	new->content = content;
 	new->next = NULL;
